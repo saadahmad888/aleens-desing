28674e3c80f4d7b96568ee1f6781c1ed33215c01
saadahmad888 <saadbhatti888@gmail.com>
2024-04-07 16:20:09 +0500

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,6 +80,26 @@ $('#meals-of-day-carsoule').owlCarousel({
   }
 });
 
+$('#include-meal-carsoule').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  dots: true,
+  autoplay:true,
+  autoplayTimeout:4000,
+  autoplayHoverPause:true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+    }
+  }
+});
 
 
 // Initialize Isotope for filtering
