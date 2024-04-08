fa8d6d5e8d7ede7242fde8884a1aa3cde61d724c
saadahmad888 <saadbhatti888@gmail.com>
2024-04-08 17:21:24 +0500

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,6 +102,32 @@ $('#include-meal-carsoule').owlCarousel({
 });
 
 
+
+$('#competitive-carsoule').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  autoplay:true,
+  autoplayTimeout:4000,
+  autoplayHoverPause:true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    }
+  }
+});
+
+
+
+
+
 // Initialize Isotope for filtering
 var $portfolio = $('.portfolio').isotope({
   itemSelector: '.item',
